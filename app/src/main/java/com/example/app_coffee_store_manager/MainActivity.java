61d10460ed39798a5ab95afce70bc8f1f9b0665c
TQT67 <trangtrinh000000@gmail.com>
2023-03-30 13:15:55 +0700

    form

---
+++ app/src/main/java/com/example/app_coffee_store_manager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_coffee_store_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
