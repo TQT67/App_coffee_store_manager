da4ed8fc9e329d2b6c4caa49be3e641f3861a4ec
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-05-15 18:43:39 +0700

    get set don dat,loai mon ..

--- app/src/main/java/com/example/app_coffee_store_manager/DTO/LoaiMonDTO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DTO/LoaiMonDTO.java
@@ -1,4 +1,33 @@
 package com.example.app_coffee_store_manager.DTO;
-
 public class LoaiMonDTO {
+
+    int MaLoai;
+    String TenLoai;
+    byte[] HinhAnh;
+
+
+    public int getMaLoai() {
+        return MaLoai;
+    }
+
+    public void setMaLoai(int maLoai) {
+        MaLoai = maLoai;
+    }
+
+    public String getTenLoai() {
+        return TenLoai;
+    }
+
+    public void setTenLoai(String tenLoai) {
+        TenLoai = tenLoai;
+    }
+
+    public byte[] getHinhAnh() {
+        return HinhAnh;
+    }
+
+    public void setHinhAnh(byte[] hinhAnh) {
+        HinhAnh = hinhAnh;
+    }
+
 }
