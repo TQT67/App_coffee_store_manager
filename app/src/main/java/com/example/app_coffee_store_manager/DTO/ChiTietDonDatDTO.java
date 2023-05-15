7bd6be9e453b3d42c9a971063803108acfa7a9aa
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-05-15 18:43:39 +0700

    get set don dat,loai mon ..

--- app/src/main/java/com/example/app_coffee_store_manager/DTO/ChiTietDonDatDTO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DTO/ChiTietDonDatDTO.java
@@ -1,4 +1,29 @@
 package com.example.app_coffee_store_manager.DTO;
-
 public class ChiTietDonDatDTO {
+
+    int MaDonDat, MaMon, SoLuong;
+
+    public int getMaDonDat() {
+        return MaDonDat;
+    }
+
+    public void setMaDonDat(int maDonDat) {
+        MaDonDat = maDonDat;
+    }
+
+    public int getMaMon() {
+        return MaMon;
+    }
+
+    public void setMaMon(int maMon) {
+        MaMon = maMon;
+    }
+
+    public int getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        SoLuong = soLuong;
+    }
 }
--- app/src/main/java/com/example/app_coffee_store_manager/DTO/DonDatDTO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DTO/DonDatDTO.java
@@ -1,4 +1,54 @@
 package com.example.app_coffee_store_manager.DTO;
-
 public class DonDatDTO {
+
+    int MaDonDat,MaBan ,MaNV;
+    String TinhTrang,NgayDat,TongTien;
+
+    public int getMaDonDat() {
+        return MaDonDat;
+    }
+
+    public void setMaDonDat(int maDonDat) {
+        MaDonDat = maDonDat;
+    }
+
+    public int getMaBan() {
+        return MaBan;
+    }
+
+    public void setMaBan(int maBan) {
+        MaBan = maBan;
+    }
+
+    public int getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(int maNV) {
+        MaNV = maNV;
+    }
+
+    public String getTinhTrang() {
+        return TinhTrang;
+    }
+
+    public void setTinhTrang(String tinhTrang) {
+        TinhTrang = tinhTrang;
+    }
+
+    public String getNgayDat() {
+        return NgayDat;
+    }
+
+    public void setNgayDat(String ngayDat) {
+        NgayDat = ngayDat;
+    }
+
+    public String getTongTien() {
+        return TongTien;
+    }
+
+    public void setTongTien(String tongTien) {
+        TongTien = tongTien;
+    }
 }
