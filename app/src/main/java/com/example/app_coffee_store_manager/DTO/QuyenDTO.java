120d8f23a956d1be85f893ac4c35ce81a9ead073
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-05-18 23:33:18 +0700

    get set quyen +thanh toan

--- app/src/main/java/com/example/app_coffee_store_manager/DTO/QuyenDTO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DTO/QuyenDTO.java
@@ -1,4 +1,24 @@
 package com.example.app_coffee_store_manager.DTO;
 
 public class QuyenDTO {
+    int MaQuyen;
+    String TenQuyen;
+
+    public int getMaQuyen() {
+        return MaQuyen;
+    }
+
+    public void setMaQuyen(int maQuyen) {
+        MaQuyen = maQuyen;
+    }
+
+    public String getTenQuyen() {
+        return TenQuyen;
+    }
+
+    public void setTenQuyen(String tenQuyen) {
+        TenQuyen = tenQuyen;
+    }
+
+
 }
