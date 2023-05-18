deae32f3d198463ab2f19e403a855860af5ad43a
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-05-18 23:33:18 +0700

    get set quyen +thanh toan

--- app/src/main/java/com/example/app_coffee_store_manager/DTO/ThanhToanDTO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DTO/ThanhToanDTO.java
@@ -1,4 +1,40 @@
 package com.example.app_coffee_store_manager.DTO;
 
-public class ThanhToanDTO   {
+public class ThanhToanDTO {
+    String TenMon;
+    int SoLuong, GiaTien;
+    byte[] HinhAnh;
+
+    public String getTenMon() {
+        return TenMon;
+    }
+
+    public void setTenMon(String tenMon) {
+        TenMon = tenMon;
+    }
+
+    public int getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        SoLuong = soLuong;
+    }
+
+    public int getGiaTien() {
+        return GiaTien;
+    }
+
+    public void setGiaTien(int giaTien) {
+        GiaTien = giaTien;
+    }
+
+    public byte[] getHinhAnh() {
+        return HinhAnh;
+    }
+
+    public void setHinhAnh(byte[] hinhAnh) {
+        HinhAnh = hinhAnh;
+    }
+
 }
