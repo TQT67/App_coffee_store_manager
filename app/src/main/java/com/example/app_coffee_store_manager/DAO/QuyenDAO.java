c26c6d08a5a1900841462677240e71b337f445a0
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-05-18 23:56:08 +0700

    create dataabase thanh toan +quyen

--- app/src/main/java/com/example/app_coffee_store_manager/DAO/QuyenDAO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DAO/QuyenDAO.java
@@ -1,4 +1,18 @@
 package com.example.app_coffee_store_manager.DAO;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import com.example.app_coffee_store_manager.DTO.QuyenDTO;
+import com.example.app_coffee_store_manager.DataBase.CreateDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 public class QuyenDAO {
+    SQLiteDatabase database;
+    public QuyenDAO(Context context){
+        CreateDatabase createDatabase = new CreateDatabase(context);
+        database = createDatabase.open();
+    }
 }
--- app/src/main/java/com/example/app_coffee_store_manager/DAO/ThanhToanDAO.java
+++ app/src/main/java/com/example/app_coffee_store_manager/DAO/ThanhToanDAO.java
@@ -1,4 +1,21 @@
 package com.example.app_coffee_store_manager.DAO;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.app_coffee_store_manager.DTO.QuyenDTO;
+import com.example.app_coffee_store_manager.DataBase.CreateDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 public class ThanhToanDAO {
+
+
+    SQLiteDatabase database;
+    public ThanhToanDAO(Context context){
+        CreateDatabase createDatabase = new CreateDatabase(context);
+        database = createDatabase.open();
+    }
 }
